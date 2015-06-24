225d6c87316c0417bc3e2fd299a5e631c998faf9
Quetzi <q@quetzi.net>
2015-06-24 22:19:14 +0100

    Fix message

--- src/main/java/net/quetzi/compasscoords/CompassEventHandler.java
+++ src/main/java/net/quetzi/compasscoords/CompassEventHandler.java
@@ -1,6 +1,8 @@
 package net.quetzi.compasscoords;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
 import net.minecraft.util.ChatComponentText;
@@ -12,12 +14,13 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 public class CompassEventHandler {
 
     @SubscribeEvent
+    @SideOnly(Side.CLIENT)
     public void onItemUse(PlayerInteractEvent event) {
 
         if (!event.world.isRemote && event.entityPlayer.getCurrentEquippedItem() != null && event.entityPlayer.getCurrentEquippedItem().getItem() == Items.compass && !event.entityPlayer.capabilities.isCreativeMode) {
             if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
                 if (event.entityPlayer instanceof EntityPlayerMP) {
-                    String message = String.format(CompassCoords.messageText, event.x, event.y, event.z);
+                    String message = String.format("<" + event.entityPlayer.getGameProfile().getName() + "> " + CompassCoords.messageText, event.x, event.y, event.z);
                     ((EntityPlayerMP) event.entityPlayer).mcServer.getConfigurationManager().sendChatMsg(new ChatComponentText(message));
                 }
             }
