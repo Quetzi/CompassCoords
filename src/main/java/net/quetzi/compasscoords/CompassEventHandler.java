52767f81b4852a00a50f3adc1eaa27d8fcbab9fc
Quetzi <q@quetzi.net>
2015-06-25 02:36:15 +0100

    Remove client side calls

--- src/main/java/net/quetzi/compasscoords/CompassEventHandler.java
+++ src/main/java/net/quetzi/compasscoords/CompassEventHandler.java
@@ -1,13 +1,10 @@
 package net.quetzi.compasscoords;
 
-import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Created by Quetzi on 12/06/15.
@@ -15,13 +12,12 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class CompassEventHandler {
 
     @SubscribeEvent
-    @SideOnly(Side.CLIENT)
     public void onItemUse(PlayerInteractEvent event) {
 
         if (!event.world.isRemote && event.entityPlayer.getCurrentEquippedItem() != null && event.entityPlayer.getCurrentEquippedItem().getItem() == Items.compass && !event.entityPlayer.capabilities.isCreativeMode) {
             if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
                 if (event.entityPlayer instanceof EntityPlayerMP) {
-                    String message = String.format("<" + Minecraft.getMinecraft().thePlayer.getName() + "> " + CompassCoords.messageText, event.pos.getX(), event.pos.getY(), event.pos.getZ());
+                    String message = String.format("<" + event.entityPlayer.getGameProfile().getName() + "> " + CompassCoords.messageText, event.pos.getX(), event.pos.getY(), event.pos.getZ());
                     ((EntityPlayerMP) event.entityPlayer).mcServer.getConfigurationManager().sendChatMsg(new ChatComponentText(message));
                 }
             }
