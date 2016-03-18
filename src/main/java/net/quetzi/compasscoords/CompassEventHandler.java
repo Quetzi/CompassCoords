194b6a3c561a9bdc08ef3987d96ddb1a3e5969a0
Quetzi <q@quetzi.net>
2016-03-18 13:25:55 +0000

    Update to 1.9

--- src/main/java/net/quetzi/compasscoords/CompassEventHandler.java
+++ src/main/java/net/quetzi/compasscoords/CompassEventHandler.java
@@ -2,7 +2,7 @@ package net.quetzi.compasscoords;
 
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
-import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -14,11 +14,11 @@ public class CompassEventHandler {
     @SubscribeEvent
     public void onItemUse(PlayerInteractEvent event) {
 
-        if (!event.world.isRemote && event.entityPlayer.getCurrentEquippedItem() != null && event.entityPlayer.getCurrentEquippedItem().getItem() == Items.compass && !event.entityPlayer.capabilities.isCreativeMode) {
+        if (!event.world.isRemote && event.entityPlayer.getHeldEquipment() != null && event.entityPlayer.getHeldEquipment() == Items.compass && !event.entityPlayer.capabilities.isCreativeMode) {
             if (event.action == PlayerInteractEvent.Action.RIGHT_CLICK_BLOCK) {
                 if (event.entityPlayer instanceof EntityPlayerMP) {
                     String message = String.format("<" + event.entityPlayer.getGameProfile().getName() + "> " + CompassCoords.messageText, event.pos.getX(), event.pos.getY(), event.pos.getZ());
-                    ((EntityPlayerMP) event.entityPlayer).mcServer.getConfigurationManager().sendChatMsg(new ChatComponentText(message));
+                    ((EntityPlayerMP) event.entityPlayer).mcServer.getPlayerList().sendChatMsg(new TextComponentString(message));
                 }
             }
         }
